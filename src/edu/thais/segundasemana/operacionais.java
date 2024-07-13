6241e0d4a313e85665aa8fdb16f0f60869bf3176
naraThais <thaisna46@gmail.com>
2024-07-12 22:58:39 -0300

    Operacionais

--- src/edu/thais/segundasemana/operacionais.java
+++ src/edu/thais/segundasemana/operacionais.java
@@ -4,5 +4,12 @@ public class operacionais {
     public static void main (String [] args){
         final int NUMERO = 2;    
         System.out.println(NUMERO);
+
+
+        int num = 2;
+
+        num = - num;
+
+        System.out.println(num);
     }
 }
