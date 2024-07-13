533a6609392fe31cd4083261eebfaa232ecd4163
naraThais <thaisna46@gmail.com>
2024-07-12 22:49:25 -0300

    Tipos

---
+++ src/edu/thais/segundasemana/operacionais.java
@@ -0,0 +1,8 @@
+package edu.thais.segundasemana;
+
+public class operacionais {
+    public static void main (String [] args){
+        final int NUMERO = 2;    
+        System.out.println(NUMERO);
+    }
+}
